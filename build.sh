902ab6e12337b54f686982ccf85201615e02e9ea
嵇晓峰 <JIXF@jixiaofengdeMacBook-Pro.local>
2020-05-22 18:27:56 +0800

    add auto build docker image script

---
+++ build.sh
@@ -0,0 +1,8 @@
+cd ../
+tar -zcvf car-detection.tar.gz car-detection/
+mkdir -p /opt/docker
+mv car-detection.tar.gz/opt/docker/
+cp car-detection/Dockerfile /opt/docker/
+cd /opt/docker/
+docker build -t car-dection:v1.0 .
+rm -f /opt/docker
