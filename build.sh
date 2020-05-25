042bf2163b5ed45aa51661e5600082cc833fbcca
嵇晓峰 <JIXF@jixiaofengdeMacBook-Pro.local>
2020-05-25 10:25:49 +0800

    修复构建镜像脚本

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ mkdir -p /opt/docker
 mv car-detection.tar.gz /opt/docker/
 cp car-detection/Dockerfile /opt/docker/
 cd /opt/docker/
-docker build -t car-dection:v1.0 .
+docker build -t car-detection:v1.0 .
 rm -rf /opt/docker
